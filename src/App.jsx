4cca0a0f5d59e8cd21d9c89815fca6371da1d084
mate1225 <mate981225@gmail.com>
2023-10-12 16:08:38 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className=" bg-slate-500 text-[2rem] text-white">this is a test</h1>
+    </>
+  );
+}
+
+export default App;
